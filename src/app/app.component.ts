9ad086c9c66beda4073a55b787ebf5a2817c2cfd
Dheeraj <malisettydheeraj@gmail.com>
2021-05-13 15:45:37 +0530

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Stormangularnode';
+}
+
